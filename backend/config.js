9c700f67d4092d20acebee462b7bb59b8c27ba9a
Aris Pratama <arispratama15@gmail.com>
2021-05-06 03:08:09 +0700

    handle auth login

---
+++ backend/config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: { /* don't expose password or any sensitive info, done only for demo */
+        host: 'localhost',
+        user: 'ris',
+        password: 'welcome@123',
+        database: 'cms_db',
+    },
+};
+
+
+module.exports = config;
